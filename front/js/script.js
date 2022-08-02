3a97c1a7dc641b31deda452b0c23f7fc043e2de3
yelmouss <yelmouss.devt@gmail.com>
2022-08-02 18:10:24 +0100

    First Push

---
+++ front/js/script.js
@@ -0,0 +1,24 @@
+// Shope depuis l'api les élements pour afficher dans notre index 
+
+fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((products) => {
+        ShowProducts(products);
+    })
+    .catch((err) => {
+        document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+        console.log("erreur 404, sur ressource api:" + err);
+    });
+
+function ShowProducts(index) {
+    let zoneArticle = document.querySelector("#items");
+    for (let article of index) {
+        zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+    <article>
+      <img src="${article.imageUrl}" alt="${article.altTxt}">
+      <h3 class="productName">${article.name}</h3>
+      <p class="productDescription">${article.description}</p>
+    </article>
+  </a>`;
+    }
+}
