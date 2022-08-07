1015752974e5f94cec2c6499b3b2e2ff609c4f73
yelmouss <yelmouss.devt@gmail.com>
2022-08-07 12:21:27 +0100

    fonction affichage autoinvoquée du numéro de commande et vide du storage lorsque l'on est sur la page confirmation

--- front/js/cart.js
+++ front/js/cart.js
@@ -499,3 +499,23 @@ function envoiPaquet() {
             });
     }
 }
+
+
+//------------------------------------------------------------
+// fonction affichage autoinvoquée du numéro de commande et vide du storage lorsque l'on est sur la page confirmation
+//------------------------------------------------------------
+(function Commande() {
+    if (page.match("confirmation")) {
+        sessionStorage.clear();
+        localStorage.clear();
+        // valeur du numero de commande
+        let numCom = new URLSearchParams(document.location.search).get("commande");
+        // merci et mise en page
+        document.querySelector("#orderId").innerHTML = `<br>${numCom}<br>Merci pour votre achat`;
+        console.log("valeur de l'orderId venant de l'url: " + numCom);
+        //réinitialisation du numero de commande
+        numCom = undefined;
+    } else {
+        console.log("sur page cart");
+    }
+})();
